94bc5d7c1e6d0fbfe2324d195cf6c0b4f52c6463
Tùng Phạm <phamduytung999@gmail.com>
2024-07-25 18:07:28 +0700

    Time: 175 ms (36.92%), Space: 18 MB (14.43%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dct = {}
+        for i in nums:
+            if i not in dct:
+                dct[i] = 1
+            else:
+                dct[i] += 1
+        major = [k for k, v in dct.items() if v > len(nums)/2]
+        return major[0]
+        
