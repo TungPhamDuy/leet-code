682030a3e87ce22e661d5c100e131dd96003950b
Tùng Phạm <phamduytung999@gmail.com>
2025-06-16 23:02:28 +0700

    Time: 1562 ms (48.52%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,25 +1,10 @@
 # Write your MySQL query statement below
-WITH CTE AS (
-    SELECT *
-    , CASE
-    WHEN INCOME < 20000 THEN 'Low Salary'
-    WHEN INCOME BETWEEN 20000 AND 50000 THEN 'Average Salary'
-    ELSE 'High Salary'
-    END AS CATEGORY
-    FROM ACCOUNTS
-)
-, CAL AS (
-    SELECT * 
-    , CASE WHEN CATEGORY = 'Low Salary' THEN 1 ELSE 0 END AS LOW
-    , CASE WHEN CATEGORY = 'Average Salary' THEN 1 ELSE 0 END AS AVERAGE
-    , CASE WHEN CATEGORY = 'High Salary' THEN 1 ELSE 0 END AS HIGH
-    FROM CTE
-)
-SELECT 'Low Salary' AS CATEGORY, SUM(LOW) AS accounts_count
-FROM CAL
-UNION
-SELECT 'Average Salary' AS CATEGORY, SUM(AVERAGE) AS accounts_count
-FROM CAL
-UNION
-SELECT 'High Salary' AS CATEGORY, SUM(HIGH) AS accounts_count
-FROM CAL
+# Tận dụng việc check boolean của SQL, 1 là true, 0 là false, tính sum để đếm
+SELECT 'Low Salary' AS CATEGORY, SUM(INCOME<20000) AS accounts_count
+FROM ACCOUNTS
+UNION 
+SELECT 'Average Salary' AS CATEGORY, SUM(INCOME>=20000 AND INCOME<=50000) AS accounts_count
+FROM ACCOUNTS
+UNION 
+SELECT 'High Salary' AS CATEGORY, SUM(INCOME>50000) AS accounts_count
+FROM ACCOUNTS
