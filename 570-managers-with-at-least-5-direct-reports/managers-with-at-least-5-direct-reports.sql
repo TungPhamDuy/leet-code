aa0215dfadd28cc98a2cdcf9b831ab51b542a13a
Tùng Phạm <phamduytung999@gmail.com>
2025-08-23 16:18:32 +0700

    Time: 292 ms (99.93%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,7 @@
 # Write your MySQL query statement below
-# tư duy:
-# 1. group by managerId lại rồi đếm tổng số dòng -> số lượng direct report
-# 2. join table vừa tính được vào main table với employee id -> trả về name của main table
-
-WITH CTE AS (
-    SELECT MANAGERID, COUNT(*) AS DIRECTREPORT
-    FROM EMPLOYEE
-    GROUP BY MANAGERID
-    HAVING DIRECTREPORT >= 5
-)
-SELECT NAME
-FROM EMPLOYEE E
-JOIN CTE C
-ON E.ID = C.MANAGERID
+SELECT E2.NAME
+FROM EMPLOYEE E1
+JOIN EMPLOYEE E2
+ON E1.MANAGERID = E2.ID
+GROUP BY E2.ID
+HAVING COUNT(E1.ID) >= 5
