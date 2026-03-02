b5313c12ff5fe355993f7ddc358a7ba7fb1dd28a
Tùng Phạm <phamduytung999@gmail.com>
2026-03-02 23:19:01 +0700

    Time: 742 ms (17.71%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,12 +1,14 @@
 # Write your MySQL query statement below
 WITH CTE AS (
-    SELECT
-    *,
-    ROW_NUMBER() OVER (PARTITION BY CUSTOMER_ID ORDER BY ORDER_DATE) AS NUM
-    FROM DELIVERY 
+    SELECT *
+    , CASE
+        WHEN order_date = customer_pref_delivery_date THEN 1
+        ELSE 0
+    END AS FLAG
+    , RANK() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS RNK
+    FROM Delivery
 )
-SELECT 
-ROUND(SUM(CASE WHEN ORDER_DATE = CUSTOMER_PREF_DELIVERY_DATE THEN 1 ELSE 0 END)/COUNT(CUSTOMER_ID)*100,2) AS IMMEDIATE_PERCENTAGE
+SELECT
+ROUND((SUM(FLAG) / COUNT(customer_id))*100,2) AS immediate_percentage
 FROM CTE
-WHERE NUM = 1
-
+WHERE RNK = 1
